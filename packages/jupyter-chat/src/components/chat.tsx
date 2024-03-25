412d4a42ebff187ad6ce0d20ffb7b740b0f71ca8
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-03-25 10:02:20 +0100

    lint

--- packages/jupyter-chat/src/components/chat.tsx
+++ packages/jupyter-chat/src/components/chat.tsx
@@ -51,7 +51,10 @@ function ChatBody({
         setMessages([]);
         return;
       } else if (message.type === 'msg') {
-        setMessages((messageGroups: IChatMessage[]) => [...messageGroups, message]);
+        setMessages((messageGroups: IChatMessage[]) => [
+          ...messageGroups,
+          message
+        ]);
       }
     }
 
