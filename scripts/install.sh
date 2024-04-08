7efe86c7cdd7493f4b39f02b297f799571c32b38
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-04-08 11:38:59 +0200

    The extension to install can be choosen in script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,14 +2,23 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
+EXTENSION=$1
+
+[ "${EXTENSION}" = "websocket" ] && PACKAGE="jupyterlab-ws-chat" || PACKAGE="jupyterlab-${EXTENSION}-chat"
+
 
 # install core packages
 pip install jupyterlab~=4.0
 jlpm install
-jlpm build
 
-# install the collaborative chat extension
-pip install -e packages/jupyterlab-collaborative-chat[test]
+if [ -z "${EXTENSION}" ]; then
+  jlpm build
+  # install the collaborative chat extension
+  pip install -e packages/jupyterlab-collaborative-chat[test]
 
-# install websocket chat extension
-pip install -e packages/jupyterlab-ws-chat[test]
+  # install websocket chat extension
+  pip install -e packages/jupyterlab-ws-chat[test]
+else
+  jlpm build:${EXTENSION}
+  pip install -e packages/${PACKAGE}[test]
+fi
