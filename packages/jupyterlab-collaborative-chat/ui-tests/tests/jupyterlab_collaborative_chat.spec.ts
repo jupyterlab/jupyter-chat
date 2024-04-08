83951465a2c8edf47bb6d32f46ce3acdf8df6d7d
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-04-08 20:15:34 +0200

    Add integration tests

--- packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
+++ packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
@@ -37,7 +37,7 @@ const fillModal = async (
   page: IJupyterLabPageFixture,
   text = '',
   button: 'Ok' | 'Cancel' = 'Ok'
-) => {
+): Promise<void> => {
   const dialog = page.getByRole('dialog');
   await dialog.getByRole('textbox').pressSequentially(text);
   await dialog.getByRole('button').filter({ hasText: button }).click();
@@ -68,6 +68,17 @@ const openSettings = async (
   }, args);
   await page.activity.activateTab('Settings');
   return (await page.activity.getPanelLocator('Settings')) as Locator;
+};
+
+const openPanel = async (page: IJupyterLabPageFixture): Promise<Locator> => {
+  const panel = page.locator('.jp-SidePanel.jp-collab-chat-sidepanel');
+
+  if (!(await panel?.isVisible())) {
+    const chatIcon = page.getByTitle('Jupyter Chat');
+    await chatIcon.click();
+    await expect(panel).toBeVisible();
+  }
+  return panel.first();
 };
 
 test.describe('#commandPalette', () => {
@@ -669,5 +680,144 @@ test.describe('#settings', () => {
     await expect(
       messages.locator('.jp-chat-message .jp-chat-rendermime-markdown')
     ).toHaveText(MSG_CONTENT + '\n');
+  });
+});
+
+test.describe('#chatPanel', () => {
+  test.describe('#initiailization', () => {
+    test('should contain the chat panel icon', async ({ page }) => {
+      const chatIcon = page.getByTitle('Jupyter Chat');
+      expect(chatIcon).toHaveCount(1);
+      expect(await chatIcon.screenshot()).toMatchSnapshot(
+        'chat_icon.png'
+      );
+    });
+
+    test('chat panel should contain a toolbar', async ({ page }) => {
+      const panel = await openPanel(page);
+      const toolbar = panel.locator('.jp-SidePanel-toolbar');
+      await expect(toolbar).toHaveCount(1);
+
+      const items = toolbar.locator('.jp-Toolbar-item');
+      await expect(items).toHaveCount(2);
+      await expect(items.first()).toHaveClass(/.jp-collab-chat-add/);
+      await expect(items.last()).toHaveClass(/.jp-collab-chat-open/);
+    });
+
+    test('chat panel should not contain a chat at init', async ({ page }) => {
+      const panel = await openPanel(page);
+      const content = panel.locator('.jp-SidePanel-content');
+      await expect(content).toBeEmpty();
+    });
+  })
+
+  test.describe('#chatCreation', () => {
+    const name = 'my-chat';
+    let panel: Locator;
+    let dialog: Locator;
+
+    test.beforeEach(async ({ page }) => {
+      panel = await openPanel(page);
+      const addButton = panel.locator(
+        '.jp-SidePanel-toolbar .jp-Toolbar-item.jp-collab-chat-add'
+      );
+      await addButton.click();
+
+      dialog = page.locator('.jp-Dialog');
+      await dialog.waitFor();
+    });
+
+    test.afterEach(async ({ page })=> {
+      for (let filename of ['untitled.chat', `${name}.chat`]) {
+        if (await page.filebrowser.contents.fileExists(filename)) {
+          await page.filebrowser.contents.deleteFile(filename);
+        }
+      }
+    });
+
+    test('should create a chat', async ({ page }) => {
+      await dialog.locator('input[type="text"]').pressSequentially(name);
+      await dialog.getByRole('button').getByText('Ok').click();
+      await page.waitForCondition(
+        async () => await page.filebrowser.contents.fileExists(`${name}.chat`)
+      );
+
+      const chatTitle = panel.locator('.jp-SidePanel-content .jp-AccordionPanel-title');
+      await expect(chatTitle).toHaveCount(1);
+      await expect(chatTitle.locator('.lm-AccordionPanel-titleLabel')).toHaveText(
+        name
+      );
+    });
+
+    test('should create an untitled file if no name is provided', async ({ page }) => {
+      await dialog.getByRole('button').getByText('Ok').click();
+      await page.waitForCondition(
+        async () => await page.filebrowser.contents.fileExists('untitled.chat')
+      );
+
+      const chatTitle = panel.locator('.jp-SidePanel-content .jp-AccordionPanel-title');
+      await expect(chatTitle).toHaveCount(1);
+      await expect(chatTitle.locator('.lm-AccordionPanel-titleLabel')).toHaveText(
+        'untitled'
+      );
+    });
+
+    test('should not create a chat if dialog is cancelled', async ({ page }) => {
+      await dialog.getByRole('button').getByText('Cancel').click();
+
+      const content = panel.locator('.jp-SidePanel-content');
+      await expect(content).toBeEmpty();
+    });
+  })
+
+  test.describe('#openingClosing', () => {
+    const name = 'my-chat';
+    let panel: Locator;
+    let select: Locator;
+
+    test.beforeEach(async ({ page }) => {
+      await page.filebrowser.contents.uploadContent('{}', 'text', `${name}.chat`);
+    });
+
+    test.afterEach(async ({ page }) => {
+      await page.filebrowser.contents.deleteFile( `${name}.chat`);
+    });
+
+    test('should list existing chat', async ({ page }) => {
+      // reload to update the chat list
+      // FIX: add listener on file creation
+      await page.reload();
+      panel = await openPanel(page);
+      select = panel.locator(
+        '.jp-SidePanel-toolbar .jp-Toolbar-item.jp-collab-chat-open select'
+      );
+
+      for (let i=0; i< await select.locator('option').count(); i++) {
+        console.log(await select.locator('option').nth(i).textContent());
+      }
+      await expect(select.locator('option')).toHaveCount(2);
+      await expect(select.locator('option').last()).toHaveText(name);
+    });
+
+    test('should open an existing chat and close it', async ({ page }) => {
+      // reload to update the chat list
+      // FIX: add listener on file creation
+      await page.reload();
+      panel = await openPanel(page);
+      select = panel.locator(
+        '.jp-SidePanel-toolbar .jp-Toolbar-item.jp-collab-chat-open select'
+      );
+
+      await select.selectOption(name);
+
+      const chatTitle = panel.locator('.jp-SidePanel-content .jp-AccordionPanel-title');
+      await expect(chatTitle).toHaveCount(1);
+      await expect(chatTitle.locator('.lm-AccordionPanel-titleLabel')).toHaveText(
+        name
+      );
+
+      await chatTitle.getByRole('button').click();
+      await expect(chatTitle).toHaveCount(0);
+    });
   });
 });
