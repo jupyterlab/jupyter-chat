76d48ed58f3fd3e2d5fab16be6c53dfe40e62c25
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-04-11 11:29:15 +0200

    Add command palette tests

--- packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
+++ packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
@@ -3,27 +3,83 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-import { expect, test } from '@jupyterlab/galata';
+import { IJupyterLabPageFixture, expect, test } from '@jupyterlab/galata';
 
-/**
- * Don't load JupyterLab webpage before running the tests.
- * This is required to ensure we capture all log messages.
- */
-test.use({ autoGoto: false });
+async function fillModal(page: IJupyterLabPageFixture, text='', button: 'Ok' | 'Cancel' = 'Ok') {
+  const dialog = page.getByRole('dialog');
+  await dialog.getByRole('textbox').pressSequentially(text);
+  await dialog.getByRole('button').filter({ hasText: button}).click();
+}
 
-test('should emit an activation console message', async ({ page }) => {
-  const logs: string[] = [];
+test.describe('#commandPalette', () => {
+  const name = 'my-chat';
 
-  page.on('console', message => {
-    logs.push(message.text());
+  test.beforeEach(async ({ page }) => {
+    await page.keyboard.press("Control+Shift+c");
   });
 
-  await page.goto();
+  test.afterEach(async ({ page }) => {
+    for (let filename of ['untitled.chat', `${name}.chat`]) {
+      if (await page.filebrowser.contents.fileExists(filename)) {
+        await page.filebrowser.contents.deleteFile(filename);
+      }
+    }
+  });
 
-  expect(
-    logs.filter(
-      s =>
-        s === 'JupyterLab extension jupyterlab-collaborative-chat is activated!'
-    )
-  ).toHaveLength(1);
+  test('should have 2 commands in palette', async ({ page }) => {
+    await expect(page.locator(
+      '#modal-command-palette li[data-command^="collaborative-chat"]'
+    )).toHaveCount(2);
+  });
+
+  test('should create a chat with name from command palette', async ({ page }) => {
+    await page
+      .locator(
+        '#modal-command-palette li[data-command="collaborative-chat:create"]'
+      )
+      .click();
+    await fillModal(page, name);
+    await page.waitForCondition(
+      async () => await page.filebrowser.contents.fileExists(`${name}.chat`)
+    );
+    await expect(page.activity.getTabLocator(`${name}.chat`)).toBeVisible();
+  });
+
+  test('should create an untitled chat from command palette', async ({ page }) => {
+    await page
+      .locator(
+        '#modal-command-palette li[data-command="collaborative-chat:create"]'
+      )
+      .click();
+    await fillModal(page);
+    await page.waitForCondition(
+      async () => await page.filebrowser.contents.fileExists('untitled.chat')
+    );
+    await expect(page.activity.getTabLocator('untitled.chat')).toBeVisible();
+  });
+
+  test('should not create a chat if modal is cancelled', async ({ page }) => {
+    await page
+      .locator(
+        '#modal-command-palette li[data-command="collaborative-chat:create"]'
+      )
+      .click();
+    await fillModal(page, '', 'Cancel');
+    const tab = page.getByRole('main').getByRole('tab');
+    await expect(tab).toHaveCount(1);
+  });
+
+  test('should open an existing chat', async ({ page }) => {
+    // Create a chat file
+    await page.filebrowser.contents.uploadContent('{}', 'text', `${name}.chat`);
+
+    // open it from command palette
+    await page
+      .locator(
+        '#modal-command-palette li[data-command="collaborative-chat:open"]'
+      )
+      .click();
+    await fillModal(page, `${name}.chat`);
+    await expect(page.activity.getTabLocator(`${name}.chat`)).toBeVisible();
+  });
 });
