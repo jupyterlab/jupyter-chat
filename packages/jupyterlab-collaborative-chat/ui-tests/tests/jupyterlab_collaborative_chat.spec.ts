6e457c8585cd168de407d7f37c0450ad8d104ac9
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-05-07 16:07:11 +0200

    Fix settings test

--- packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
+++ packages/jupyterlab-collaborative-chat/ui-tests/tests/jupyterlab_collaborative_chat.spec.ts
@@ -82,7 +82,15 @@ const openSettings = async (
   await page.evaluate(async args => {
     await window.jupyterapp.commands.execute('settingeditor:open', args);
   }, args);
-  await page.activity.activateTab('Settings');
+
+  // Activate the settings tab, sometimes it does not automatically.
+  const settingsTab = page
+    .getByRole('main')
+    .getByRole('tab', { name: 'Settings', exact: true });
+  await settingsTab.click();
+  await page.waitForCondition(
+    async () => (await settingsTab.getAttribute('aria-selected')) === 'true'
+  );
   return (await page.activity.getPanelLocator('Settings')) as Locator;
 };
 
