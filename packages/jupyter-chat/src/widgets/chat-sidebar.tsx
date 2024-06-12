a6b5fa52265586200c4dc4d766fe436bcf32d655
Nicolas Brichet <32258950+brichet@users.noreply.github.com>
2024-06-12 17:46:20 +0200

    Use the Chat.IOptions as option for the Widget and Sidebar (#55)
    * Use the Chat.IOptions as option for the Widget and Sidebar
    
    * Fix options in websocket chat
    
    * Explicitelly pass the model to the chat widget
    
    * Clarify comment

--- packages/jupyter-chat/src/widgets/chat-sidebar.tsx
+++ packages/jupyter-chat/src/widgets/chat-sidebar.tsx
@@ -3,26 +3,14 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-import { IThemeManager, ReactWidget } from '@jupyterlab/apputils';
-import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+import { ReactWidget } from '@jupyterlab/apputils';
 import React from 'react';
 
 import { Chat } from '../components/chat';
 import { chatIcon } from '../icons';
-import { IChatModel } from '../model';
 
-export function buildChatSidebar(
-  chatModel: IChatModel,
-  themeManager: IThemeManager | null,
-  rmRegistry: IRenderMimeRegistry
-): ReactWidget {
-  const ChatWidget = ReactWidget.create(
-    <Chat
-      model={chatModel}
-      themeManager={themeManager}
-      rmRegistry={rmRegistry}
-    />
-  );
+export function buildChatSidebar(options: Chat.IOptions): ReactWidget {
+  const ChatWidget = ReactWidget.create(<Chat {...options} />);
   ChatWidget.id = 'jupyter-chat::side-panel';
   ChatWidget.title.icon = chatIcon;
   ChatWidget.title.caption = 'Jupyter Chat'; // TODO: i18n
