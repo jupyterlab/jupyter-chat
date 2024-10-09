f75b710631a0acc20ce7144dfeb60d722c90f86f
Nicolas Brichet <32258950+brichet@users.noreply.github.com>
2024-10-09 10:54:49 +0200

    Refactor UI tests (#86)
    * Move side panel tests
    
    * Move navigation tests
    
    * Move send message test
    
    * Move the notifications tests
    
    * Update Playwright Snapshots
    
    * Move message toolbar tests
    
    * Group tests on unread messages
    
    * lint
    
    * Move config related tests
    
    * Update snapshots
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- python/jupyterlab-collaborative-chat/ui-tests/tests/test-utils.ts
+++ python/jupyterlab-collaborative-chat/ui-tests/tests/test-utils.ts
@@ -39,6 +39,22 @@ export const openChat = async (
   return (await page.activity.getPanelLocator(filename)) as Locator;
 };
 
+export const openChatToSide = async (
+  page: IJupyterLabPageFixture,
+  filename: string
+): Promise<Locator> => {
+  const panel = page.locator('.jp-SidePanel.jp-collab-chat-sidepanel');
+  await page.evaluate(async filepath => {
+    const inSidePanel = true;
+    await window.jupyterapp.commands.execute('collaborative-chat:open', {
+      filepath,
+      inSidePanel
+    });
+  }, filename);
+  await page.waitForCondition(() => panel.isVisible());
+  return panel;
+};
+
 export const sendMessage = async (
   page: IJupyterLabPageFixture,
   filename: string,
@@ -70,4 +86,24 @@ export const splitMainArea = async (
   await page.mouse.down();
   await page.mouse.move(viewerBBox!.x + 0.5 * viewerBBox!.width, 600);
   await page.mouse.up();
+};
+
+export const openSettings = async (
+  page: IJupyterLabPageFixture,
+  globalSettings?: boolean
+): Promise<Locator> => {
+  const args = globalSettings ? {} : { query: 'Chat' };
+  await page.evaluate(async args => {
+    await window.jupyterapp.commands.execute('settingeditor:open', args);
+  }, args);
+
+  // Activate the settings tab, sometimes it does not automatically.
+  const settingsTab = page
+    .getByRole('main')
+    .getByRole('tab', { name: 'Settings', exact: true });
+  await settingsTab.click();
+  await page.waitForCondition(
+    async () => (await settingsTab.getAttribute('aria-selected')) === 'true'
+  );
+  return (await page.activity.getPanelLocator('Settings')) as Locator;
 };
