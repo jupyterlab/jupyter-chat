d2d255be8ab3d966b16dc0b071658ee790573616
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-12-06 17:17:57 +0100

    Add integration tests with notebook

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -10,12 +10,28 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
-  webServer: {
-    command: 'jlpm start',
-    url: 'http://localhost:8888/lab',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI
-  },
+  projects: [
+    {
+      name: 'jupyterlab',
+      webServer: {
+        command: 'jlpm start',
+        url: 'http://localhost:8888/lab',
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI
+      },
+      testIgnore: 'tests/notebook-application.spec.ts'
+    },
+    {
+      name: 'notebook',
+      webServer: {
+        command: 'jlpm start:notebook',
+        url: 'http://localhost:8888/tree',
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI
+      },
+      testMatch: 'tests/notebook-application.spec.ts'
+    }
+  ],
   use: {
     contextOptions: {
       permissions: ['clipboard-read', 'clipboard-write']
