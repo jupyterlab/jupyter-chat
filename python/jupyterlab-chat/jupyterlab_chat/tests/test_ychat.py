7572baf44d0280d1a2ab2c878d8b543b72080870
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-12-10 22:56:05 +0100

    Fix indentation in tests file

--- python/jupyterlab-chat/jupyterlab_chat/tests/test_ychat.py
+++ python/jupyterlab-chat/jupyterlab_chat/tests/test_ychat.py
@@ -13,136 +13,136 @@ from ..models import User, Message
 from ..ychat import YChat
 
 USER = User(
-  username=str(uuid4()),
-  name="Test user",
-  display_name="Test user"
+    username=str(uuid4()),
+    name="Test user",
+    display_name="Test user"
 )
 
 USER2 = User(
-  username=str(uuid4()),
-  name="Test user 2",
-  display_name="Test user 2"
+    username=str(uuid4()),
+    name="Test user 2",
+    display_name="Test user 2"
 )
 
 
 def create_message():
-  return Message(
-    type="msg",
-    id=str(uuid4()),
-    body="This is a test message",
-    time=time.time(),
-    sender=USER.username
-  )
+    return Message(
+        type="msg",
+        id=str(uuid4()),
+        body="This is a test message",
+        time=time.time(),
+        sender=USER.username
+    )
 
 
 def test_initialize_ychat():
-  chat = YChat()
-  assert chat._get_messages() == []
-  assert chat._get_users() == {}
-  assert chat.get_metadata() == {}
+    chat = YChat()
+    assert chat._get_messages() == []
+    assert chat._get_users() == {}
+    assert chat.get_metadata() == {}
 
 
 def test_add_user():
-  chat = YChat()
-  chat.set_user(USER)
-  assert USER.username in chat._get_users().keys()
-  assert chat._get_users()[USER.username] == asdict(USER)
+    chat = YChat()
+    chat.set_user(USER)
+    assert USER.username in chat._get_users().keys()
+    assert chat._get_users()[USER.username] == asdict(USER)
 
 
 def test_get_user_type():
-  chat = YChat()
-  chat.set_user(USER)
-  assert isinstance(chat.get_user(USER.username), User)
+    chat = YChat()
+    chat.set_user(USER)
+    assert isinstance(chat.get_user(USER.username), User)
 
 
 def test_get_user():
-  chat = YChat()
-  chat.set_user(USER)
-  chat.set_user(USER2)
-  assert chat.get_user(USER.username) == USER
-  assert chat.get_user(USER2.username) == USER2
-  assert chat.get_user(str(uuid4())) == None
+    chat = YChat()
+    chat.set_user(USER)
+    chat.set_user(USER2)
+    assert chat.get_user(USER.username) == USER
+    assert chat.get_user(USER2.username) == USER2
+    assert chat.get_user(str(uuid4())) == None
 
 
 def test_get_user_by_name_type():
-  chat = YChat()
-  chat.set_user(USER)
-  assert isinstance(chat.get_user_by_name(USER.name), User)
+    chat = YChat()
+    chat.set_user(USER)
+    assert isinstance(chat.get_user_by_name(USER.name), User)
 
 
 def test_get_user_by_name():
-  chat = YChat()
-  chat.set_user(USER)
-  chat.set_user(USER2)
-  assert chat.get_user_by_name(USER.name) == USER
-  assert chat.get_user_by_name(USER2.name) == USER2
-  assert chat.get_user_by_name(str(uuid4())) == None
+    chat = YChat()
+    chat.set_user(USER)
+    chat.set_user(USER2)
+    assert chat.get_user_by_name(USER.name) == USER
+    assert chat.get_user_by_name(USER2.name) == USER2
+    assert chat.get_user_by_name(str(uuid4())) == None
 
 
 def test_add_message():
-  chat = YChat()
-  msg = create_message()
-  chat.add_message(msg)
-  assert len(chat._get_messages()) == 1
-  assert chat._get_messages()[0] == asdict(msg)
+    chat = YChat()
+    msg = create_message()
+    chat.add_message(msg)
+    assert len(chat._get_messages()) == 1
+    assert chat._get_messages()[0] == asdict(msg)
 
 
 def test_get_message_type():
-  chat = YChat()
-  msg = create_message()
-  chat.add_message(msg)
-  assert isinstance(chat.get_message(msg.id)[0], Message)
+    chat = YChat()
+    msg = create_message()
+    chat.add_message(msg)
+    assert isinstance(chat.get_message(msg.id)[0], Message)
 
 
 def test_get_message():
-  chat = YChat()
-  msg = create_message()
-  chat.add_message(msg)
-  assert chat.get_message(msg.id) == (msg, 0)
+    chat = YChat()
+    msg = create_message()
+    chat.add_message(msg)
+    assert chat.get_message(msg.id) == (msg, 0)
 
 
 def test_set_message_should_add():
-  chat = YChat()
-  msg = create_message()
-  chat.set_message(msg)
-  assert len(chat._get_messages()) == 1
-  assert chat._get_messages()[0] == asdict(msg)
+    chat = YChat()
+    msg = create_message()
+    chat.set_message(msg)
+    assert len(chat._get_messages()) == 1
+    assert chat._get_messages()[0] == asdict(msg)
 
 
 def test_set_message_should_update():
-  chat = YChat()
-  msg = create_message()
-  index = chat.add_message(msg)
-  msg.body = "Updated content"
-  chat.set_message(msg, index)
-  assert len(chat._get_messages()) == 1
-  assert chat._get_messages()[0] == asdict(msg)
+    chat = YChat()
+    msg = create_message()
+    index = chat.add_message(msg)
+    msg.body = "Updated content"
+    chat.set_message(msg, index)
+    assert len(chat._get_messages()) == 1
+    assert chat._get_messages()[0] == asdict(msg)
 
 
 def test_set_message_should_add_with_new_id():
-  chat = YChat()
-  msg = create_message()
-  index = chat.add_message(msg)
-  new_msg = Message(**asdict(msg))
-  new_msg.id = str(uuid4())
-  new_msg.body = "Updated content"
-  chat.set_message(new_msg, index)
-  assert len(chat._get_messages()) == 2
-  assert chat._get_messages()[0] == asdict(msg)
-  assert chat._get_messages()[1] == asdict(new_msg)
+    chat = YChat()
+    msg = create_message()
+    index = chat.add_message(msg)
+    new_msg = Message(**asdict(msg))
+    new_msg.id = str(uuid4())
+    new_msg.body = "Updated content"
+    chat.set_message(new_msg, index)
+    assert len(chat._get_messages()) == 2
+    assert chat._get_messages()[0] == asdict(msg)
+    assert chat._get_messages()[1] == asdict(new_msg)
 
 
 def test_set_message_should_update_with_wrong_index():
-  chat = YChat()
-  msg = create_message()
-  chat.add_message(msg)
-  new_msg = create_message()
-  new_msg.body = "New content"
-  index = chat.add_message(new_msg)
-  assert index == 1
-  new_msg.body = "Updated content"
-  chat.set_message(new_msg, 0)
-  assert len(chat._get_messages()) == 2
-  assert chat._get_messages()[0] == asdict(msg)
-  assert chat._get_messages()[1] == asdict(new_msg)
+    chat = YChat()
+    msg = create_message()
+    chat.add_message(msg)
+    new_msg = create_message()
+    new_msg.body = "New content"
+    index = chat.add_message(new_msg)
+    assert index == 1
+    new_msg.body = "Updated content"
+    chat.set_message(new_msg, 0)
+    assert len(chat._get_messages()) == 2
+    assert chat._get_messages()[0] == asdict(msg)
+    assert chat._get_messages()[1] == asdict(new_msg)
 
