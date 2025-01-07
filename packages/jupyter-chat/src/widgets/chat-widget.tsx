527b52d7de321826c25def5e8290243101a5c336
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-01-07 16:22:26 +0100

    Focus input when clicking in a chat, on a non focussable element

--- packages/jupyter-chat/src/widgets/chat-widget.tsx
+++ packages/jupyter-chat/src/widgets/chat-widget.tsx
@@ -14,11 +14,12 @@ export class ChatWidget extends ReactWidget {
   constructor(options: Chat.IOptions) {
     super();
 
-    this.id = 'jupyter-chat::widget';
     this.title.icon = chatIcon;
     this.title.caption = 'Jupyter Chat'; // TODO: i18n
 
     this._chatOptions = options;
+    this.id = `jupyter-chat::widget::${options.model.name}`;
+    this.node.onclick = () => this.model.focusInput();
   }
 
   /**
