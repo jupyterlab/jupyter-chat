5109c40f23820d2f8907a0e5a6b2b8c88c8d5612
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-03-26 11:08:59 +0100

    lint

--- ui-tests/tests/input-toolbar.spec.ts
+++ ui-tests/tests/input-toolbar.spec.ts
@@ -5,22 +5,31 @@
 import { PathExt } from '@jupyterlab/coreutils';
 import { expect, test } from '@jupyterlab/galata';
 
-import { createChat, exposeDepsJs, getPlugin, openChat, openChatToSide } from './test-utils';
+import {
+  createChat,
+  exposeDepsJs,
+  getPlugin,
+  openChat,
+  openChatToSide
+} from './test-utils';
 
 const CHAT = 'test.chat';
 
 test.describe('#inputToolbar', () => {
-  test('Should remove toolbar item for main area chat', async ({ page, tmpPath }) => {
+  test('Should remove toolbar item for main area chat', async ({
+    page,
+    tmpPath
+  }) => {
     // Expose a function to get a plugin.
     await page.evaluate(exposeDepsJs({ getPlugin }));
 
     // Modify the input toolbar when a chat is opened.
     await page.evaluate(async () => {
-      const tracker = (await window.getPlugin(
-        'jupyterlab-chat-extension:factory'
-      )).tracker;
+      const tracker = (
+        await window.getPlugin('jupyterlab-chat-extension:factory')
+      ).tracker;
 
-      const updateToolbar = (registry) => {
+      const updateToolbar = registry => {
         registry.removeItem('attach');
       };
 
@@ -40,14 +49,18 @@ test.describe('#inputToolbar', () => {
     const chatPanel = await openChat(page, chatPath);
 
     // The main area chat input should not contain the 'attach' button.
-    const inputToolbar = chatPanel.locator('.jp-chat-input-container .jp-chat-input-toolbar');
+    const inputToolbar = chatPanel.locator(
+      '.jp-chat-input-container .jp-chat-input-toolbar'
+    );
     await expect(inputToolbar).toBeVisible();
     await expect(inputToolbar.locator('button')).toHaveCount(2);
     expect(inputToolbar.locator('.jp-chat-attach-button')).not.toBeAttached();
 
     // The side panel chat input should contain the 'attach' button.
     const chatSidePanel = await openChatToSide(page, chatPath);
-    const inputToolbarSide = chatSidePanel.locator('.jp-chat-input-container .jp-chat-input-toolbar');
+    const inputToolbarSide = chatSidePanel.locator(
+      '.jp-chat-input-container .jp-chat-input-toolbar'
+    );
     await expect(inputToolbarSide).toBeVisible();
     await expect(inputToolbarSide.locator('button')).toHaveCount(3);
     expect(inputToolbarSide.locator('.jp-chat-attach-button')).toBeAttached();
