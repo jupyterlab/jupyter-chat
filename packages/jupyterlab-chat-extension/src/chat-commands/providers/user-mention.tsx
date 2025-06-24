2d992aa8af0f91ac6f255e0f71621edacfe26982
David L. Qiu <david@qiu.dev>
2025-06-24 13:14:13 -0700

    update mention command to use user.mention_name

--- packages/jupyterlab-chat-extension/src/chat-commands/providers/user-mention.tsx
+++ packages/jupyterlab-chat-extension/src/chat-commands/providers/user-mention.tsx
@@ -27,31 +27,50 @@ export const mentionCommandsPlugin: JupyterFrontEndPlugin<void> = {
 class MentionCommandProvider implements IChatCommandProvider {
   public id: string = 'jupyter-chat:mention-commands';
 
-  // Regex used to find all mentions in a message.
-  // IMPORTANT: the global flag must be specified to find >1 mentions.
-  private _regex: RegExp = /@[\w-]*/g;
+  /**
+   * Regex that matches all mentions in a message. The first capturing group
+   * contains the mention name of the mentioned user.
+   *
+   * IMPORTANT: the 'g' flag must be set to return multiple `@`-mentions when
+   * parsing the entire input `inputModel.value`.
+   */
+  private _regex: RegExp = /@([\w-]*)/g;
 
   /**
    * Lists all valid user mentions that complete the current word.
    */
   async listCommandCompletions(inputModel: IInputModel) {
-    // Return early if the current word does not match the expected syntax
-    const match = inputModel.currentWord?.match(this._regex)?.[0];
-    if (!match) {
+    const { currentWord } = inputModel;
+    if (!currentWord) {
       return [];
     }
 
-    // Otherwise, build the list of `@`-mention commands that complete the
-    // current word.
-    const existingMentions = new Set(inputModel.value?.match(this._regex));
+    // Get current mention, which will always be the first captured group in the
+    // first match.
+    // `matchAll()` is used here because `match()` does not return captured
+    // groups when the regex is global.
+    const currentMention = Array.from(
+      currentWord.matchAll(this._regex)
+    )?.[0]?.[1];
+
+    // Return early if no user is currently mentioned
+    if (currentMention === undefined || currentMention === null) {
+      return [];
+    }
+
+    // Otherwise, build the list of users that complete `currentMention`, and
+    // return them as command completions
+    const existingMentions = this._getExistingMentions(inputModel);
     const commands: ChatCommand[] = Array.from(this._getUsers(inputModel))
       // remove users whose mention names that do not complete the match
-      .filter(user => user[0].toLowerCase().startsWith(match.toLowerCase()))
+      .filter(user =>
+        user[0].toLowerCase().startsWith(currentMention.toLowerCase())
+      )
       // remove users already mentioned in the message
       .filter(user => !existingMentions.has(user[0]))
       .map(user => {
         return {
-          name: user[0],
+          name: '@' + user[0],
           providerId: this.id,
           icon: user[1].icon,
           spaceOnAccept: true
@@ -62,15 +81,39 @@ class MentionCommandProvider implements IChatCommandProvider {
   }
 
   /**
+   * Returns the set of mention names that have already been @-mentioned in the
+   * input.
+   */
+  _getExistingMentions(inputModel: IInputModel): Set<string> {
+    const matches = inputModel.value?.matchAll(this._regex);
+    const existingMentions = new Set<string>();
+    for (const match of matches) {
+      const mention = match?.[1];
+      // ignore if 1st group capturing the mention name is an empty string
+      if (!mention) {
+        continue;
+      }
+      existingMentions.add(mention);
+    }
+
+    return existingMentions;
+  }
+
+  /**
    * Adds all users identified via `@` as mentions to the new message
    * immediately prior to submission.
    */
   async onSubmit(inputModel: IInputModel) {
     const input = inputModel.value;
-    const matches = input.match(this._regex) ?? [];
+    const matches = input.matchAll(this._regex);
 
     for (const match of matches) {
-      const mentionedUser = this._getUsers(inputModel).get(match);
+      const mention = match?.[1];
+      if (!mention) {
+        continue;
+      }
+
+      const mentionedUser = this._getUsers(inputModel).get(mention);
       if (mentionedUser) {
         inputModel.addMention?.(mentionedUser.user);
       }
@@ -85,13 +128,7 @@ class MentionCommandProvider implements IChatCommandProvider {
     const users = new Map();
     const userList = inputModel.chatContext.users;
     userList.forEach(user => {
-      let mentionName = user.mention_name;
-      if (!mentionName) {
-        mentionName = Private.getMentionName(user);
-        user.mention_name = mentionName;
-      }
-
-      users.set(mentionName, {
+      users.set(user.mention_name, {
         user,
         icon: <Avatar user={user} />
       });
@@ -106,15 +143,4 @@ namespace Private {
     user: IUser;
     icon: JSX.Element | null;
   };
-
-  /**
-   * Build the mention name from a User object.
-   */
-  export function getMentionName(user: IUser): string {
-    const username = (user.display_name ?? user.name ?? user.username).replace(
-      / /g,
-      '-'
-    );
-    return `@${username}`;
-  }
 }
