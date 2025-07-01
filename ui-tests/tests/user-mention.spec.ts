4fb7454eaa8375718f73ccb060a138aec1701855
Piyush Jain <piyushjain@duck.com>
2025-06-30 18:38:58 -0700

    Fixed tests

--- ui-tests/tests/user-mention.spec.ts
+++ ui-tests/tests/user-mention.spec.ts
@@ -19,30 +19,68 @@ test.use({
 });
 
 test.describe('#user-mention', () => {
-  test.beforeEach(async ({ page }) => {
-    // Create chat file
-    await page.filebrowser.contents.uploadContent('{}', 'text', FILENAME);
-  });
+  let guestPage: IJupyterLabPageFixture;
+   test.beforeEach(
+    async ({ baseURL, browser, page, tmpPath, waitForApplication }) => {
+      await page.filebrowser.contents.uploadContent('{}', 'text', FILENAME);
+      // Create a new user.
+      const user2: Partial<User.IUser> = {
+        identity: {
+          username: 'jovyan_2',
+          name: 'jovyan_2',
+          display_name: 'jovyan_2',
+          initials: 'JP',
+          color: 'var(--jp-collaborator-color2)'
+        }
+      };
+
+      // Create a new page for guest.
+      const { page: newPage } = await galata.newPage({
+        baseURL: baseURL!,
+        browser,
+        mockUser: user2,
+        tmpPath,
+        waitForApplication
+      });
+      await newPage.evaluate(() => {
+        // Acknowledge any dialog
+        window.galata.on('dialog', d => {
+          d?.resolve();
+        });
+      });
+      guestPage = newPage;
+    }
+  );
 
   test.afterEach(async ({ page }) => {
+    guestPage.close();
     if (await page.filebrowser.contents.fileExists(FILENAME)) {
       await page.filebrowser.contents.deleteFile(FILENAME);
     }
   });
-
-  test('should open chat command', async ({ page }) => {
+  
+  test('should open chat command with guest user', async ({ page }) => {
     const chatPanel = await openChat(page, FILENAME);
+
+    // Send a message from guest to make sure users are populated
+    await sendMessage(guestPage, FILENAME, 'Hello from guest');
+    
+    // Check mentions on main page
     const input = chatPanel
       .locator('.jp-chat-input-container')
       .getByRole('combobox');
     const chatCommandName = page.locator('.jp-chat-command-name');
     await input.press('@');
     await expect(chatCommandName).toHaveCount(1);
-    expect(await chatCommandName.nth(0).textContent()).toBe('@jovyan');
+    expect(await chatCommandName.nth(0).textContent()).toBe('@jovyan_2');
   });
 
   test('should format mention in message', async ({ page }) => {
     const chatPanel = await openChat(page, FILENAME);
+
+    // Send a message from guest.
+    await sendMessage(guestPage, FILENAME, 'test');
+
     const input = chatPanel
       .locator('.jp-chat-input-container')
       .getByRole('combobox');
@@ -50,16 +88,17 @@ test.describe('#user-mention', () => {
       '.jp-chat-input-container .jp-chat-send-button'
     );
     const chatCommandName = page.locator('.jp-chat-command-name');
+    
     await input.press('@');
     await chatCommandName.nth(0).click();
 
-    await expect(input).toContainText('@jovyan');
+    await expect(input).toContainText('@jovyan_2');
     await sendButton.click();
 
     const message = chatPanel.locator('.jp-chat-messages-container').nth(0);
     await expect(message).toBeAttached();
     expect(message.locator('.jp-chat-mention')).toBeAttached();
-    expect(message.locator('.jp-chat-mention')).toContainText('@jovyan');
+    expect(message.locator('.jp-chat-mention')).toContainText('@jovyan_2');
   });
 });
 
@@ -90,7 +129,7 @@ test.describe('#current-user-filter', () => {
       });
       await newPage.evaluate(() => {
         // Acknowledge any dialog
-        window.galataip.on('dialog', d => {
+        window.galata.on('dialog', d => {
           d?.resolve();
         });
       });
@@ -117,11 +156,9 @@ test.describe('#current-user-filter', () => {
       .locator('.jp-chat-input-container')
       .getByRole('combobox');
     
-    // Type @ to trigger mention list
-    await input.press('@');
-    
     // Wait for the mention list to appear
     const chatCommandName = page.locator('.jp-chat-command-name');
+    await input.press('@');
     await expect(chatCommandName).toBeVisible();
     
     // There should only be one user in the list (jovyan_2) 
@@ -158,7 +195,7 @@ test.describe('#collaborator-mention', () => {
       });
       await newPage.evaluate(() => {
         // Acknowledge any dialog
-        window.galataip.on('dialog', d => {
+        window.galata.on('dialog', d => {
           d?.resolve();
         });
       });
@@ -185,12 +222,8 @@ test.describe('#collaborator-mention', () => {
     const chatCommandName = page.locator('.jp-chat-command-name');
 
     await input.press('@');
-    await expect(chatCommandName).toHaveCount(2);
-    for (let i = 0; i < (await chatCommandName.count()); i++) {
-      expect(['@jovyan', '@jovyan_2']).toContain(
-        await chatCommandName.nth(i).textContent()
-      );
-    }
+    await expect(chatCommandName).toHaveCount(1);
+    expect(await chatCommandName.nth(0).textContent()).toBe('@jovyan_2');
   });
 
   test('should not be case sensitive', async ({ page }) => {
@@ -205,11 +238,7 @@ test.describe('#collaborator-mention', () => {
     const chatCommandName = page.locator('.jp-chat-command-name');
 
     await input.pressSequentially('@JO');
-    await expect(chatCommandName).toHaveCount(2);
-    for (let i = 0; i < (await chatCommandName.count()); i++) {
-      expect(['@jovyan', '@jovyan_2']).toContain(
-        await chatCommandName.nth(i).textContent()
-      );
-    }
+    await expect(chatCommandName).toHaveCount(1);
+    expect(await chatCommandName.nth(0).textContent()).toBe('@jovyan_2');
   });
 });
