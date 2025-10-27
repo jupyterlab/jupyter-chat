0993b77eaf44f311c0f6d30663d3995a6e39dc25
Brian E. Granger <ellisonbg@gmail.com>
2025-10-27 11:34:25 -0700

    Editing of messages is broken, comment out until we can fix the behavior

--- packages/jupyter-chat/src/components/messages/toolbar.tsx
+++ packages/jupyter-chat/src/components/messages/toolbar.tsx
@@ -3,7 +3,7 @@
  * Distributed under the terms of the Modified BSD License.
  */
 
-import EditIcon from '@mui/icons-material/Edit';
+// import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Box, IconButton, Tooltip } from '@mui/material';
 import React from 'react';
@@ -16,27 +16,27 @@ const TOOLBAR_CLASS = 'jp-chat-toolbar';
 export function MessageToolbar(props: MessageToolbar.IProps): JSX.Element {
   const buttons: JSX.Element[] = [];
 
-  if (props.edit !== undefined) {
-    const editButton = (
-      <Tooltip key="edit" title="Edit" placement="top" arrow>
-        <span>
-          <IconButton
-            onClick={props.edit}
-            aria-label="Edit"
-            sx={{
-              width: '24px',
-              height: '24px',
-              padding: 0,
-              lineHeight: 0
-            }}
-          >
-            <EditIcon sx={{ fontSize: '16px' }} />
-          </IconButton>
-        </span>
-      </Tooltip>
-    );
-    buttons.push(editButton);
-  }
+  // if (props.edit !== undefined) {
+  //   const editButton = (
+  //     <Tooltip key="edit" title="Edit" placement="top" arrow>
+  //       <span>
+  //         <IconButton
+  //           onClick={props.edit}
+  //           aria-label="Edit"
+  //           sx={{
+  //             width: '24px',
+  //             height: '24px',
+  //             padding: 0,
+  //             lineHeight: 0
+  //           }}
+  //         >
+  //           <EditIcon sx={{ fontSize: '16px' }} />
+  //         </IconButton>
+  //       </span>
+  //     </Tooltip>
+  //   );
+  //   buttons.push(editButton);
+  // }
   if (props.delete !== undefined) {
     const deleteButton = (
       <Tooltip key="delete" title="Delete" placement="top" arrow>
