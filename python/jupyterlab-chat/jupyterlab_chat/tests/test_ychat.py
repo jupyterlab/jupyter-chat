87512601fa3d037da1d52d9dcda16e6e649532ba
Abigayle-Mercer <abigaylemercer@gmail.com>
2025-12-03 09:16:35 -0800

    fixed the test naming of callback

--- python/jupyterlab-chat/jupyterlab_chat/tests/test_ychat.py
+++ python/jupyterlab-chat/jupyterlab_chat/tests/test_ychat.py
@@ -9,7 +9,7 @@ import pytest
 from uuid import uuid4
 from ..models import message_asdict_factory, Message, NewMessage, User
 from ..ychat import YChat
-from ..utils import find_mentions_callback
+from ..utils import find_mentions
 
 USER = User(
     username=str(uuid4()),
@@ -181,7 +181,7 @@ def test_update_message_includes_mentions():
     assert msg.mentions == [USER2.username]
 
     msg.body = f"@{USER3.mention_name} Goodbye!"
-    chat.update_message(msg, trigger_actions=[find_mentions_callback])
+    chat.update_message(msg, trigger_actions=[find_mentions])
     updated_msg = chat.get_message(msg_id)
     assert updated_msg
     assert updated_msg.mentions == [USER3.username]
@@ -200,7 +200,7 @@ def test_update_message_append_includes_mentions():
     assert msg.mentions == [USER2.username]
 
     msg.body = f" and @{USER3.mention_name}!"
-    chat.update_message(msg, append=True, trigger_actions=[find_mentions_callback])
+    chat.update_message(msg, append=True, trigger_actions=[find_mentions])
     updated_msg = chat.get_message(msg_id)
     assert updated_msg
     assert sorted(updated_msg.mentions) == sorted([USER2.username, USER3.username])
@@ -218,7 +218,7 @@ def test_update_message_append_no_duplicate_mentions():
     assert msg.mentions == [USER2.username]
 
     msg.body = f" @{USER2.mention_name} again!"
-    chat.update_message(msg, append=True, trigger_actions=[find_mentions_callback])
+    chat.update_message(msg, append=True, trigger_actions=[find_mentions])
     updated_msg = chat.get_message(msg_id)
     assert updated_msg
     assert updated_msg.mentions == [USER2.username]
