0c23f2fce2d0fc8629245544b481c7bb5f1ab0e9
Nicolas Brichet <32258950+brichet@users.noreply.github.com>
2026-04-22 14:19:47 +0200

    Add a placeholder widget in side panel (#409)
    * Create a default placeholder widget
    
    * Add tests
    
    * lint
    
    * Add a placeholder factory and a token to provide it from an extension
    
    * Add jest tests on placeholder factory
    
    * Update the ne chat button and use of translator
    
    * Add trnaslation bundle in placeholder options

---
+++ packages/jupyter-chat/src/widgets/placeholder.tsx
@@ -0,0 +1,122 @@
+/*
+ * Copyright (c) Jupyter Development Team.
+ * Distributed under the terms of the Modified BSD License.
+ */
+
+import { IRenderMime } from '@jupyterlab/rendermime';
+import {
+  addIcon,
+  ReactWidget,
+  ToolbarButtonComponent
+} from '@jupyterlab/ui-components';
+import { ISignal } from '@lumino/signaling';
+import React, { useEffect, useState } from 'react';
+
+import { useTranslator } from '../context';
+
+/**
+ * The default placeholder widget.
+ */
+export class defaultPlaceholder extends ReactWidget {
+  constructor(options: Placeholder.IProps) {
+    super();
+    this._props = options;
+  }
+
+  render() {
+    return <PlaceholderComponent {...this._props} />;
+  }
+
+  private _props: Placeholder.IProps;
+}
+
+/**
+ * The placeholder namespace.
+ */
+export namespace Placeholder {
+  /**
+   * The options of the placeholder widget.
+   */
+  export interface IProps {
+    /**
+     * The initial chat names.
+     */
+    chatNames: { [name: string]: string };
+    /**
+     * A callback to open an existing chat by name.
+     *
+     * @param name - the display name of the chat to open.
+     */
+    open: (name: string) => Promise<void>;
+    /**
+     * A callback to create and open a new chat.
+     */
+    onCreate?: () => Promise<void>;
+    /**
+     * An optional signal emitting when the chat list changes.
+     */
+    chatNamesChanged?: ISignal<any, { [name: string]: string }>;
+    /**
+     * An optional translation bundle.
+     */
+    trans?: IRenderMime.TranslationBundle;
+  }
+}
+
+/**
+ * The default placeholder component.
+ */
+const PlaceholderComponent = (props: Placeholder.IProps): JSX.Element => {
+  const trans = props.trans ?? useTranslator();
+
+  const [chatNames, setChatNames] = useState<{ [name: string]: string }>(
+    props.chatNames
+  );
+
+  useEffect(() => {
+    if (!props.chatNamesChanged) {
+      return;
+    }
+    const onChanged = (_: any, names: { [name: string]: string }) => {
+      setChatNames(names);
+    };
+    props.chatNamesChanged.connect(onChanged);
+    return () => {
+      props.chatNamesChanged!.disconnect(onChanged);
+    };
+  }, [props.chatNamesChanged]);
+
+  const names = Object.keys(chatNames).sort((a, b) => a.localeCompare(b));
+
+  return (
+    <div className="jp-chat-placeholder">
+      <h3>{trans.__('No chat opened')}</h3>
+      {props.onCreate && (
+        <div className="jp-chat-placeholder-hint">
+          <ToolbarButtonComponent
+            label={trans.__('New chat')}
+            icon={addIcon}
+            onClick={props.onCreate}
+            tooltip={trans.__('Create a new chat')}
+            className="jp-chat-add"
+          />
+        </div>
+      )}
+      {names.length > 0 && (
+        <div className="jp-chat-placeholder-list">
+          <p>{trans.__('Open an existing chat:')}</p>
+          {names.map(name => (
+            <div key={name}>
+              <button
+                className="jp-chat-placeholder-chat-item"
+                onClick={() => props.open(name)}
+              >
+                {name}
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
