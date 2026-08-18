6602452f6cba952d72332977e1bcc2306a386577
Nicolas Brichet <nicolas.brichet@quantstack.net>
2026-08-18 11:59:20 +0200

    Trigger a rerender of the message when the mime model only is updated

--- packages/jupyterlab-chat/src/model.ts
+++ packages/jupyterlab-chat/src/model.ts
@@ -501,7 +501,8 @@ export class LabChatModel
               'raw_time',
               'deleted',
               'edited',
-              'metadata'
+              'metadata',
+              'mime_model'
             ].includes(key)
           ) {
             const update: Partial<IMessageContent> = {};
